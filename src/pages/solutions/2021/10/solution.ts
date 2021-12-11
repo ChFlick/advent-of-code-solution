3fb820ff233bf7746546c74151db8d90323d3b8f
Christoph Flick <ChFlick@users.noreply.github.com>
2021-12-11 15:47:31 +0100

    Added solution for 2021-10.2

--- src/pages/solutions/2021/10/solution.ts
+++ src/pages/solutions/2021/10/solution.ts
@@ -2,7 +2,7 @@ import { Solution } from '@/solutions/Solution.interface';
 
 export const solution: Solution = {
   benchmarkRuns: 5,
-  first: (input: string) => {
+  first(input: string) {
     const BRACKET_PAIRS: Record<string, string> = {
       '(': ')',
       '[': ']',
@@ -35,5 +35,44 @@ export const solution: Solution = {
       }
       return sum;
     }, 0);
+  },
+  second(input: string) {
+    const BRACKET_PAIRS: Record<string, string> = {
+      '(': ')',
+      '[': ']',
+      '{': '}',
+      '<': '>',
+    };
+
+    const VALUES: Record<string, number> = {
+      ')': 1,
+      ']': 2,
+      '}': 3,
+      '>': 4,
+    };
+
+    const incompletes = input.split('\n').reduce((incompletes: string[][], line) => {
+      const stack: string[] = [];
+      for (const char of line) {
+        if (Object.keys(BRACKET_PAIRS).includes(char)) {
+          stack.push(char);
+        }
+        else {
+          const openingBracket = stack.pop();
+          if (!openingBracket) {
+            throw new Error('No opening bracket found');
+          }
+
+          if (char !== BRACKET_PAIRS[openingBracket] && Object.values(BRACKET_PAIRS).includes(char)) {
+            return incompletes;
+          }
+        }
+      }
+      return [...incompletes, stack.map(b => BRACKET_PAIRS[b]).reverse()];
+    }, []);
+    const incompleteValues = incompletes
+      .map(incomplete => incomplete.reduce((sum, bracket) => sum * 5 + VALUES[bracket], 0))
+      .sort((a, b) => a - b);
+    return incompleteValues[(incompleteValues.length - 1) / 2];
   },
 };
